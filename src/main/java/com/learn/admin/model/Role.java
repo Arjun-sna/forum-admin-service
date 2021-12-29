da55fd168ee42e5f0ed9e8de98a01120bc1f63fa
Arjun-sna <arj.sna@gmail.com>
2021-12-29 20:21:42 +0530

    restructure role

--- src/main/java/com/learn/admin/model/Role.java
+++ src/main/java/com/learn/admin/model/Role.java
@@ -18,24 +18,25 @@ public class Role extends Auditable {
 
     private String name;
     private int accountId;
+    private String permissions;
 
-    private boolean createUser;
-    private boolean editUser;
-    private boolean archiveUser;
-
-    private boolean createPost;
-    private boolean editPost;
-    private boolean deletePost;
-    private boolean hidePost;
-
-    private boolean createTopic;
-    private boolean editTopic;
-    private boolean deleteTopic;
-    private boolean hideTopic;
-
-    private boolean viewHidden;
-
-    private boolean createRole;
-    private boolean editRole;
-    private boolean deleteRole;
+//    private boolean createUser;
+//    private boolean editUser;
+//    private boolean archiveUser;
+//
+//    private boolean createPost;
+//    private boolean editPost;
+//    private boolean deletePost;
+//    private boolean hidePost;
+//
+//    private boolean createTopic;
+//    private boolean editTopic;
+//    private boolean deleteTopic;
+//    private boolean hideTopic;
+//
+//    private boolean viewHidden;
+//
+//    private boolean createRole;
+//    private boolean editRole;
+//    private boolean deleteRole;
 }
