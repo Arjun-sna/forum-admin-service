31167a40dda833ad8b5b9c0c1a24cbd5b0687234
Arjun-sna <arj.sna@gmail.com>
2021-12-29 22:45:10 +0530

    fix user data structure

--- src/main/java/com/learn/admin/payload/CreateUserData.java
+++ src/main/java/com/learn/admin/payload/CreateUserData.java
@@ -3,14 +3,11 @@ package com.learn.admin.payload;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class CreateUserData extends UserData {
-    @NotBlank
-    private int accountId;
-
-    @NotBlank
+    @Positive(message = "Invalid role id")
     private int roleId;
 }
