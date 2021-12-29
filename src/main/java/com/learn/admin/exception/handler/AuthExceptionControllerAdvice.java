5c24afc93e904f4c8a6d3b8b197e4469a485d0e7
Arjun-sna <arj.sna@gmail.com>
2021-12-29 18:53:58 +0530

    handle exception

--- src/main/java/com/learn/admin/exception/handler/AuthExceptionControllerAdvice.java
+++ src/main/java/com/learn/admin/exception/handler/AuthExceptionControllerAdvice.java
@@ -4,6 +4,7 @@ import com.learn.admin.exception.AuthContextException;
 import com.learn.admin.exception.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,5 +23,12 @@ public class AuthExceptionControllerAdvice {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(new ErrorResponse(HttpStatus.UNAUTHORIZED.toString(), ex.getMessage(), null));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException ex) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(new ErrorResponse(HttpStatus.FORBIDDEN.toString(), ex.getMessage(), null));
     }
 }
