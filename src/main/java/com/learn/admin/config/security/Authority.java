762512827ee18fd4be2a9dcdcebad6754940ce7f
Arjun-sna <arj.sna@gmail.com>
2021-12-29 18:52:21 +0530

    add authority class

--- src/main/java/com/learn/admin/config/security/Authority.java
+++ src/main/java/com/learn/admin/config/security/Authority.java
@@ -1,2 +1,14 @@
-package com.learn.admin.config.security;public class Permission {
+package com.learn.admin.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@RequiredArgsConstructor(staticName = "of")
+public class Authority implements GrantedAuthority {
+    private final Permission permission;
+
+    @Override
+    public String getAuthority() {
+        return permission.value();
+    }
 }
