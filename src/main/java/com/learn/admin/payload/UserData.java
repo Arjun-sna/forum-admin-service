c953f597ba07756891cda0b8b1141a7afff7fa39
Arjun-sna <arj.sna@gmail.com>
2021-12-29 20:22:38 +0530

    change migration

--- src/main/java/com/learn/admin/payload/UserData.java
+++ src/main/java/com/learn/admin/payload/UserData.java
@@ -1,2 +1,33 @@
-package com.learn.admin.payload;public class UserData {
+package com.learn.admin.payload;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+public class UserData {
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String username;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String lastName;
+
+    @NotBlank
+    @Pattern(
+            regexp = "(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]{2,}$)",
+            message = "Should be a valid email address"
+    )
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 16)
+    private String password;
 }
