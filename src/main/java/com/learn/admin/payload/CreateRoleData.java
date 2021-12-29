819ef2d738dc520f76205ec8efb1070fe05f7c47
Arjun-sna <arj.sna@gmail.com>
2021-12-29 20:22:09 +0530

    restructure roledata

--- src/main/java/com/learn/admin/payload/CreateRoleData.java
+++ src/main/java/com/learn/admin/payload/CreateRoleData.java
@@ -1,10 +1,13 @@
 package com.learn.admin.payload;
 
+import com.learn.admin.config.security.Permission;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Data
 @Builder
@@ -12,44 +15,15 @@ public class CreateRoleData {
     @Size(min = 3, max = 20)
     private String name;
 
-    private boolean createUser;
-    private boolean editUser;
-    private boolean archiveUser;
-
-    private boolean createPost;
-    private boolean editPost;
-    private boolean deletePost;
-    private boolean hidePost;
-
-    private boolean createTopic;
-    private boolean editTopic;
-    private boolean deleteTopic;
-    private boolean hideTopic;
-
-    private boolean viewHidden;
-
-    private boolean createRole;
-    private boolean editRole;
-    private boolean deleteRole;
+    @NotNull
+    @Size(min = 1)
+    private ArrayList<Permission> permissions;
 
     public static CreateRoleData createAdminRole() {
+        ArrayList<Permission> adminPermission = new ArrayList<>(Arrays.asList(Permission.values()));
         return CreateRoleData.builder()
                 .name("Admin")
-                .createUser(true)
-                .editUser(true)
-                .archiveUser(true)
-                .createPost(true)
-                .editPost(true)
-                .deletePost(true)
-                .hidePost(true)
-                .createTopic(true)
-                .editTopic(true)
-                .deleteTopic(true)
-                .hideTopic(true)
-                .viewHidden(true)
-                .createRole(true)
-                .editRole(true)
-                .deleteRole(true)
+                .permissions(adminPermission)
                 .build();
     }
 }
