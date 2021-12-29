807f1ac826c3a5c77dfed8d6b1eff5b29f3ce09e
Arjun-sna <arj.sna@gmail.com>
2021-12-29 20:21:03 +0530

    update permissions

--- src/main/java/com/learn/admin/config/security/Permission.java
+++ src/main/java/com/learn/admin/config/security/Permission.java
@@ -3,8 +3,17 @@ package com.learn.admin.config.security;
 import java.util.stream.Stream;
 
 public enum Permission {
-    CAN_CREATE_USER("can_create_user"),
-    CAN_EDIT_USER("can_edit_user");
+    CAN_CREATE_USER("create_user"),
+    CAN_ARCHIVE_USER("archive_user"),
+    CAN_EDIT_USER("edit_user"),
+    HAS_POST_ADMIN_ACCESS("post_admin_access"),
+    HAS_POST_ACCESS("post_access"),
+    HAS_TOPIC_ADMIN_ACCESS("topic_admin_access"),
+    HAS_TOPIC_ACCESS("topic_access"),
+    CAN_VIEW_HIDDEN("view_hidden"),
+    CAN_CREATE_ROLE("create_role"),
+    CAN_DELETE_ROLE("delete_role"),
+    CAN_EDIT_ROLE("edit_role");
 
     private final String permission;
     private static final Permission[] VALUES;
