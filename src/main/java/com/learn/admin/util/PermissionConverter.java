40a1c8444372310bfedf48ca45461deb60409c01
Arjun-sna <arj.sna@gmail.com>
2021-12-31 19:32:44 +0530

    refactor code

---
+++ src/main/java/com/learn/admin/util/PermissionConverter.java
@@ -0,0 +1,16 @@
+package com.learn.admin.util;
+
+import com.learn.admin.dto.UserSort;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
+
+public class UserSortConverter implements Converter<String, UserSort> {
+    @Override
+    public UserSort convert(@NonNull String source) {
+        try {
+            return UserSort.of(source);
+        } catch (IllegalArgumentException ex) {
+            return UserSort.FIRSTNAME;
+        }
+    }
+}
