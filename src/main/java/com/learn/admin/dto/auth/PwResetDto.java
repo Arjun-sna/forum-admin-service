4303759635de33de2dae5a3a214dd6e3a3a562ec
Arjun-sna <arj.sna@gmail.com>
2022-01-14 16:25:11 +0530

    add validation

--- src/main/java/com/learn/admin/dto/auth/PwResetDto.java
+++ src/main/java/com/learn/admin/dto/auth/PwResetDto.java
@@ -3,9 +3,11 @@ package com.learn.admin.dto.auth;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class PwResetDto {
     @Email
+    @NotNull
     private String email;
 }
