7c5f52aab63046c8e00749361f6cadae388d60a9
Arjun-sna <arj.sna@gmail.com>
2022-01-16 20:51:58 +0530

    add kafka config

--- src/main/java/com/learn/admin/kafka/KafkaTopicConfig.java
+++ src/main/java/com/learn/admin/kafka/KafkaTopicConfig.java
@@ -1,2 +1,15 @@
-package com.learn.admin.kafka;public class KafkaConfig {
+package com.learn.admin.kafka;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class KafkaTopicConfig {
+    @Value("${spring.kafka.topics.pw_reset_notification}")
+    private String pwResetNotificationTopic;
+
+    @Value("${spring.kafka.topics.account_activation_notification}")
+    private String accountActivationNotificationTopic;
 }
