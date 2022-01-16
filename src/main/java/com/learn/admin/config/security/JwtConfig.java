d9f11b8990b42ecce078ec54d1c8f7ec54886889
Arjun <arj.sna@gmail.com>
2022-01-16 23:58:16 +0530

    refactor jwt config

--- src/main/java/com/learn/admin/config/security/JwtConfig.java
+++ src/main/java/com/learn/admin/config/security/JwtConfig.java
@@ -1,28 +1,17 @@
 package com.learn.admin.config.security;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 @Data
-// TODO: 16/01/22 use configuration properties annotation
+@ConfigurationProperties(prefix = "security.jwt")
 public class JwtConfig {
-    @Value("${security.jwt.prefix}")
     private String prefix;
-
-    @Value("${security.jwt.auth_token_expiration}")
     private int authTokenExpiration;
-
-    @Value("${security.jwt.pw_reset_token_expiration}")
     private int pwResetTokenExpiration;
-
-    @Value("${security.jwt.account_activation_token_expiration}")
     private int accountActivationTokenExpiration;
-
-    @Value("${security.jwt.secret}")
     private String secret;
-
-    @Value("${security.jwt.issuer}")
     private String issuer;
 }
