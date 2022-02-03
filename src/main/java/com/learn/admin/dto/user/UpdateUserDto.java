edd04931eb3a80346cac4ff3e016ac5d0e3a74cf
Arjun <arj.sna@gmail.com>
2022-02-03 20:11:02 +0530

    add dto

--- src/main/java/com/learn/admin/dto/user/UpdateUserDto.java
+++ src/main/java/com/learn/admin/dto/user/UpdateUserDto.java
@@ -1,2 +1,14 @@
-package com.learn.admin.dto.user;public class UpdateUserDto {
+package com.learn.admin.dto.user;
+
+import lombok.Data;
+
+import javax.validation.constraints.Size;
+
+@Data
+public class UpdateUserDto {
+    @Size(min = 3, max = 100)
+    private String firstName;
+
+    @Size(min = 2, max = 100)
+    private String lastName;
 }
